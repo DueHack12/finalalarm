b781b14720d24d49922cd79cd871a588f6c7880d
Jack Singewald <63979352+DueHack12@users.noreply.github.com>
2024-04-24 07:55:06 -0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-Alarm_Clear = 0
-
 def on_button_pressed_ab():
-    global Alarm_Clear
-    Alarm_Clear = 1
-    music.stop_all_sounds()
-    basic.clear_screen()
+    radio.send_string("Alarm Cleared")
+    control.reset()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+radio.set_transmit_power(7)
+radio.set_group(5)
+radio.send_string("ARMED")
+radio.set_frequency_band(37)
 
 def on_forever():
     if input.light_level() >= 25 or input.is_gesture(Gesture.SHAKE):
+        Alarm_Clear = 0
         music.play(music.string_playable("C5 D C5 D C5 D C5 D ", 120),
             music.PlaybackMode.LOOPING_IN_BACKGROUND)
+        radio.send_string("ALARM")
         while Alarm_Clear == 0:
             basic.show_leds("""
                 # # # # #
--- main.ts
+++ main.ts
@@ -1,45 +1,54 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 65) {
+        control.reset()
+    }
+})
 input.onButtonPressed(Button.AB, function () {
-    Alarm_Clear = 1
-    music.stopAllSounds()
-    basic.clearScreen()
     radio.sendString("Alarm Cleared")
+    control.reset()
 })
 let Alarm_Clear = 0
+radio.setTransmitPower(7)
 radio.setGroup(5)
 radio.sendString("ARMED")
+radio.setFrequencyBand(37)
+basic.pause(1000)
+basic.showString("ARMED")
 basic.forever(function () {
-    if (input.lightLevel() >= 25 || input.isGesture(Gesture.Shake)) {
-        music.play(music.stringPlayable("C5 D C5 D C5 D C5 D ", 120), music.PlaybackMode.LoopingInBackground)
-        radio.sendString("ALARM")
-        while (Alarm_Clear == 0) {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
+    while ((input.lightLevel() >= 129 || input.isGesture(Gesture.Shake)) == true) {
+        if (input.lightLevel() >= 129 || input.isGesture(Gesture.Shake)) {
+            music.play(music.stringPlayable("C5 C B D C5 C B D ", 120), music.PlaybackMode.LoopingInBackground)
+            radio.sendString("ALARM")
+            while (Alarm_Clear == 0) {
+                basic.showLeds(`
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+            }
         }
     }
 })
