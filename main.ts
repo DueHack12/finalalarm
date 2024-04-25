5bc7820ffbfc6c53cbb0a79c78a761dd026effa3
Jack Singewald <63979352+DueHack12@users.noreply.github.com>
2024-04-25 08:21:17 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,8 +17,8 @@ radio.setFrequencyBand(37)
 basic.pause(1000)
 basic.showString("ARMED")
 basic.forever(function () {
-    while ((input.lightLevel() >= 25 || input.isGesture(Gesture.Shake)) == true) {
-        if (input.lightLevel() >= 25 || input.isGesture(Gesture.Shake)) {
+    while ((input.lightLevel() >= 10 || input.isGesture(Gesture.Shake)) == true) {
+        if (input.lightLevel() >= 10 || input.isGesture(Gesture.Shake)) {
             music.play(music.stringPlayable("C5 C B D C5 C B D ", 120), music.PlaybackMode.LoopingInBackground)
             radio.sendString("ALARM")
             while (Alarm_Clear == 0) {
